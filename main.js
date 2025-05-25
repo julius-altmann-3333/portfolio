84cc3e23b782bf02057c6d1ddccb45c0c3a5b4d0
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-05-25 14:14:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function openModal(url) {
+		document.getElementById("modal").style.display = "flex";
+		document.getElementById("modal-iframe").src = url;
+	}
 
+	function closeModal() {
+		document.getElementById("modal").style.display = "none";
+		document.getElementById("modal-iframe").src = "";
+	}
